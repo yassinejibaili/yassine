fb0bc72828f5e2af9df64710260403261e2f7022
yassinejibaili <loser.455.yj@gmail.com>
2024-05-05 14:15:29 +0100

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Role;
+use Hash;
+use Spatie\Permission\Traits\HasRoles;
+use DB;
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user= User::create([
+            'name' => 'Admin',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('123456'),
+        ]);
+        $role = Role::create([
+            'slug' => 'admin',
+            'name' => 'Adminstrator',
+        ]);
+        $user->roles()->sync($role->id);
+        
+    }
+}
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+     ©  {{date('Y')}} Attendance Management System <span class="d-none d-sm-inline-block"> - Crafted by yassine </span>.
+</footer>
